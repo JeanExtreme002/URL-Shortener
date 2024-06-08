766320ce218a63949256ad7a2696f4a7be7d6940
JeanExtreme002 <jeangamerextreme@gmail.com>
2024-06-08 18:54:37 -0300

    added start script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,26 +1,33 @@
-console.log("Try npm run lint/fix!");
+console.log('Try npm run lint/fix!');
 
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const longString =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
 
-const trailing = 'Semicolon'
+const trailing = 'Semicolon';
 
-			const why={am:'I tabbed?'};
+const why = {am: 'I tabbed?'};
 
-const iWish = "I didn't have a trailing space..."; 
+const iWish = "I didn't have a trailing space...";
 
-const sicilian = true;;
+const sicilian = true;
 
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
+const vizzini = sicilian ? !sicilian : sicilian;
 
-const re = /foo   bar/;
+const re = /foo {3}bar/;
 
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!Boolean(andThose.length)) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
+export function doSomeStuff(
+  withThis: string,
+  andThat: string,
+  andThose: string[]
+) {
+  //function on one line
+  if (!andThose.length) {
+    return false;
+  }
+  console.log(withThis);
+  console.log(andThat);
+  console.dir(andThose);
+  console.log(longString, trailing, why, iWish, vizzini, re);
+  return;
 }
 // TODO: more examples
